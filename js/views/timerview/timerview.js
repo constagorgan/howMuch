600b7cae99f0aa5a73fda778ed129439be2ae41a
Justin Atanasiu <justin.atanasiu@gmail.com>
2017-02-23 15:34:07 +0200

    Time left until / Time passed since / Time left of (during event), based on duration and event date

--- js/views/timerview/timerview.js
+++ js/views/timerview/timerview.js
@@ -9,134 +9,148 @@ define([
   'text!../../../templates/timerview/timerview.html',
   'views/common/chatview'
 ], function ($, _, moment, countdown, Backbone, timerviewTemplate, ChatView) {
-  'use strict'
+      'use strict'
 
-  var timeZones = [moment.tz('Europe/Athens'), moment.tz('Europe/London'), moment.tz('Europe/Berlin')];
-  var timezone = moment.tz(moment.tz.guess());
-  var deadline;
-  var timeinterval = setInterval(function () {}, 1000);;
-  var initialOffset = timezone._offset;
-  var getEvent = Backbone.Model.extend({
-    idAttribute: '_id',
-    initialize: function () {
-      $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
-        options.crossDomain = {
-          crossDomain: true
-        }
+      var timeZones = [moment.tz('Europe/Athens'), moment.tz('Europe/London'), moment.tz('Europe/Berlin')];
+      var timezone = moment.tz(moment.tz.guess());
+      var deadline;
+      var duration;
+      var timeinterval = setInterval(function () {}, 1000);;
+      var initialOffset = timezone._offset;
+      var getEvent = Backbone.Model.extend({
+        idAttribute: '_id',
+        initialize: function () {
+          $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
+            options.crossDomain = {
+              crossDomain: true
+            }
+          })
+        },
+        urlRoot: 'http://localhost:8003/getEvent'
       })
-    },
-    urlRoot: 'http://localhost:8003/getEvent'
-  })
 
-  var TimerviewView = Backbone.View.extend({
-    render: function () {
-      var template = _.template(timerviewTemplate)
-      this.$el.html(template)
-      this.$el.append(this.chatView.$el)
-      this.chatView.render()
-      return this
-    },
-    initialize: function (options) {
-      this.chatView = new ChatView(options)
-      var event = new getEvent();
-      event.fetch({
-        data: options
-      }).done(function (results) {
-        if(!results || !results.length){
-        //          show no event found with this name; aici o sa apara not found si daca nu are acces la ea sau nu exista.
+      var TimerviewView = Backbone.View.extend({
+        render: function () {
+          var template = _.template(timerviewTemplate)
+          this.$el.html(template)
+          this.$el.append(this.chatView.$el)
+          this.chatView.render()
+          return this
+        },
+        initialize: function (options) {
+          this.chatView = new ChatView(options)
+          var event = new getEvent();
+          event.fetch({
+            data: options
+          }).done(function (results) {
+            if (!results || !results.length) {
+              //          show no event found with this name; aici o sa apara not found si daca nu are acces la ea sau nu exista.
+            }
+            var response = results[0]
+
+            // trebuie atentie pt ca trebuie sa existe un exemplu pentru fiecare timezone
+            // pt ambele variante daylight si non daylight. trebuie parcursa lista din moment-timezone iar al treilea camp indica UTC
+            // unde sunt doua numere, alea reprezinta variantele pt ora de vara si ora de iarna
+            // in orice combinatie, trebuie sa existe o optiune si numai una pt fiecare timezone               
+            var localTimezone = _.findIndex(timeZones, function (zone) {
+              return zone._offeset = timezone._offset;
+            });
+            timeZones[localTimezone] = timezone;
+            $('#utcText').text('UTC ' + getNumber(timezone._offset / 60) + ' - ' + timezone._z.name);
+            deadline = new Date(response.eventDate);
+            duration = response.duration; //duration in miliseconds
+            initializeClock('clockdiv', initialOffset, deadline, duration);
+            $('#eventName').text(response.Name);
+          });
+        },
+        events: {
+          'click #utcChangeLeft': 'utcChangeLeft',
+          'click #utcChangeRight': 'utcChangeRight'
+        },
+        utcChangeRight: function (e) {
+          var selectedTimezoneIndex = _.findIndex(timeZones, function (zone) {
+            return zone._offset === timezone._offset;
+          });
+          if (selectedTimezoneIndex + 1 < timeZones.length)
+            timezone = timeZones[selectedTimezoneIndex + 1];
+          else
+            timezone = timeZones[0];
+          $('#utcText').text('UTC ' + getNumber(timezone._offset / 60) + ' - ' + timezone._z.name);
+          var date = new Date();
+          date.off
+          initializeClock('clockdiv', timezone._offset, deadline, duration);
+        },
+        utcChangeLeft: function (e) {
+          var selectedTimezoneIndex = _.findIndex(timeZones, function (zone) {
+            return zone._offset === timezone._offset;
+          });
+          if (selectedTimezoneIndex - 1 >= 0)
+            timezone = timeZones[selectedTimezoneIndex - 1];
+          else
+            timezone = timeZones[timeZones.length - 1];
+          $('#utcText').text('UTC ' + getNumber(timezone._offset / 60) + ' - ' + timezone._z.name);
+          initializeClock('clockdiv', timezone._offset, deadline, duration);
         }
-        var response = results[0]
-        
-        // trebuie atentie pt ca trebuie sa existe un exemplu pentru fiecare timezone
-        // pt ambele variante daylight si non daylight. trebuie parcursa lista din moment-timezone iar al treilea camp indica UTC
-        // unde sunt doua numere, alea reprezinta variantele pt ora de vara si ora de iarna
-        // in orice combinatie, trebuie sa existe o optiune si numai una pt fiecare timezone               
-        var localTimezone = _.findIndex(timeZones, function (zone) {
-          return zone._offeset = timezone._offset;
-        });
-        timeZones[localTimezone] = timezone;
-        $('#utcText').text('UTC ' + getNumber(timezone._offset / 60) + ' - ' + timezone._z.name);
-        deadline = new Date(response.eventDate);
-        initializeClock('clockdiv', initialOffset, deadline);
-        $('#eventName').text(response.Name);
-      });
-    },
-    events: {
-      'click #utcChangeLeft': 'utcChangeLeft',
-      'click #utcChangeRight': 'utcChangeRight'
-    },
-    utcChangeRight: function (e) {
-      var selectedTimezoneIndex = _.findIndex(timeZones, function (zone) {
-        return zone._offset === timezone._offset;
-      });
-      if (selectedTimezoneIndex + 1 < timeZones.length)
-        timezone = timeZones[selectedTimezoneIndex + 1];
-      else
-        timezone = timeZones[0];
-      $('#utcText').text('UTC ' + getNumber(timezone._offset / 60) + ' - ' + timezone._z.name);
-      var date = new Date();
-      date.off
-      initializeClock('clockdiv', timezone._offset, deadline);
-    },
-    utcChangeLeft: function (e) {
-      var selectedTimezoneIndex = _.findIndex(timeZones, function (zone) {
-        return zone._offset === timezone._offset;
-      });
-      if (selectedTimezoneIndex - 1 >= 0)
-        timezone = timeZones[selectedTimezoneIndex - 1];
-      else
-        timezone = timeZones[timeZones.length - 1];
-      $('#utcText').text('UTC ' + getNumber(timezone._offset / 60) + ' - ' + timezone._z.name);
-      initializeClock('clockdiv', timezone._offset, deadline);
-    }
 
-  })
+      })
 
-  function getNumber(theNumber) {
-    if (theNumber > 0) {
-      return "+" + theNumber;
-    } else {
-      return theNumber.toString();
-    }
-  }
+      function getNumber(theNumber) {
+        if (theNumber > 0) {
+          return "+" + theNumber;
+        } else {
+          return theNumber.toString();
+        }
+      }
 
-  function initializeClock(id, offset, endtime) {
-    clearInterval(timeinterval)
-    var clock = document.getElementById(id);
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
-    var monthsSpan = clock.querySelector('.months');
-    var weeksSpan = clock.querySelector('.weeks');
-    var yearsSpan = clock.querySelector('.years');
-    var t;
+      function initializeClock(id, offset, eventDate, duration) {
+        clearInterval(timeinterval)
+        var clock = document.getElementById(id);
+        var daysSpan = clock.querySelector('.days');
+        var hoursSpan = clock.querySelector('.hours');
+        var minutesSpan = clock.querySelector('.minutes');
+        var secondsSpan = clock.querySelector('.seconds');
+        var monthsSpan = clock.querySelector('.months');
+        var weeksSpan = clock.querySelector('.weeks');
+        var yearsSpan = clock.querySelector('.years');
+        var t;
 
-    function updateClock() {
-      var starttime = new Date((new Date()).getTime() + (offset - initialOffset) * 60 * 1000);
-      t = countdown(starttime, endtime, countdown.YEARS | countdown.MONTHS | countdown.WEEKS | countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
-      daysSpan.innerHTML = t.days;
-      weeksSpan.innerHTML = t.weeks;
-      monthsSpan.innerHTML = t.months;
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-      yearsSpan.innerHTML = t.years;
-      var x = moment.tz.names;
+        function updateClock() {
+          var now = new Date((new Date()).getTime() + (offset - initialOffset) * 60 * 1000);
+          if (now < eventDate) {
+            t = countdown(now, eventDate, countdown.YEARS | countdown.MONTHS | countdown.WEEKS | countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
+            $('#timeTitle').text('Time Left Until');
+          } else if (now >= eventDate) {
+            var eventDateWithDuration = new Date(eventDate.getTime() + parseInt(duration) + (offset - initialOffset) * 60 * 1000);
+            if (now < eventDateWithDuration) {
+                t = countdown(now, eventDateWithDuration, countdown.YEARS | countdown.MONTHS | countdown.WEEKS | countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
+                $('#timeTitle').text('Time left of');
+              } else {
+                $('#timeTitle').text('Time passed since');
+                 t = countdown(eventDateWithDuration, now, countdown.YEARS | countdown.MONTHS | countdown.WEEKS | countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
+              }
+            }
+            daysSpan.innerHTML = t.days;
+            weeksSpan.innerHTML = t.weeks;
+            monthsSpan.innerHTML = t.months;
+            hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+            yearsSpan.innerHTML = t.years;
+            var x = moment.tz.names;
 
-      if (!t.years)
-        $('#yearsCol').hide();
-      if (!t.years && !t.months)
-        $('#monthsCol').hide();
-      if (!t.years && !t.months && !t.weeks)
-        $('#weeksCol').hide();
+            if (!t.years)
+              $('#yearsCol').hide();
+            if (!t.years && !t.months)
+              $('#monthsCol').hide();
+            if (!t.years && !t.months && !t.weeks)
+              $('#weeksCol').hide();
 
-    }
-    updateClock();
-    timeinterval = setInterval(function () {
-      updateClock();
-    }, 1000);
-  }
+          }
+          updateClock();
+          timeinterval = setInterval(function () {
+            updateClock();
+          }, 1000);
+        }
 
-  return TimerviewView
-})
+        return TimerviewView
+      })
