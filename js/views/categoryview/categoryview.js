feb45782e073b66c1be1ef90728cced704cfb348
ConstaGorgan <consta.gorgan@gmail.com>
2017-02-27 04:48:56 +0200

    Category page autocomplete & small code clean-up

--- js/views/categoryview/categoryview.js
+++ js/views/categoryview/categoryview.js
@@ -45,10 +45,39 @@ define([
       this.$el.html(template({
         categoryName: this.categoryName
       }))
+      addHandlers()
       return this
     }
 
   })
 
+  // nu merge drop down-ul aici, trebuie verificat
+  function addHandlers() {
+    $("#search-input").autocomplete({
+      source: function (request, response) {
+        var event = new ws.searchEvents();
+        event.fetch({
+          data: {
+            name: request.term
+          }
+        }).done(function (resp) {
+          response(_.map(resp, function (e) {
+            return {
+              id: e.id,
+              label: e.name
+            };
+          }));
+        })
+      },
+      minLength: 1,
+      select: function (event, ui) {
+        var url = ui.item.label;
+        if (url != '#') {
+           Backbone.history.navigate('#event/' + encodeURIComponent(ui.item.label) + '/' + ui.item.id, true)
+        }
+      }
+    })
+  }
+  
   return CategoryviewView
 })
