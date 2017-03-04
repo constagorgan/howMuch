30008abdbb16c3544c36e27cbe6c2863d491c572
Justin Atanasiu <justin.atanasiu@gmail.com>
2017-03-04 23:07:13 +0200

    get upcoming events backend add parameters

--- php/getUpcomingEvents.php
+++ php/getUpcomingEvents.php
@@ -4,32 +4,61 @@ class GetUpcomingEvent {
   public static function getUpcoming(){
     $index = '';
     $categoryId = '';
+    $orderType = '';
+    $name = '';
     if(isset ( $_GET["index"] ))
       $index = $_GET['index'];
     if(isset ( $_GET["categoryId"] ))
       $categoryId = $_GET['categoryId'];
+    if(isset ( $_GET["orderType"] ))
+      $orderType = $_GET['orderType'];
+    if(isset ( $_GET["name"] ))
+      $name = $_GET['name'];
+    
+    $object=new stdClass();
     
     header("Access-Control-Allow-Origin: *");
     // connect to the mysql database
     include_once('config.inc.php');
     $link = mysqli_connect($myUltimateSecret, $myBiggerSecret, $myExtremeSecret, $mySecret);
     mysqli_set_charset($link,'utf8');
+    $sqlFirstQuery = "";
+    $sqlSecondQuery = "select count(*) as totalResults from (";
+      
     $sql = "select * from events INNER JOIN categories_map on events.id = categories_map.event_id WHERE eventDate >= CURDATE() ";
-
+      
     if($categoryId != '')
-      $sql .= "AND categories_map.category_id='$categoryId' GROUP BY events.id ORDER BY events.counter DESC LIMIT 10 "; 
-    else 
-      $sql .= "GROUP BY events.id ORDER BY eventDate ASC LIMIT 10 ";
-
+      $sql .= "AND categories_map.category_id='$categoryId' "; 
+    
+    if($name != '')
+      $sql .= "AND Name LIKE '%$name%' ";    
+    
+    $sql .= "GROUP BY events.id ";
+    
+    if($orderType != ''){
+      if($orderType == 'popular')
+        $sql .= "ORDER BY events.counter DESC ";
+      else if ($orderType == 'chronological')
+        $sql .= "ORDER BY eventDate ASC ";
+      else if ($orderType == 'alphabetical')
+        $sql .= "ORDER BY events.name ASC ";
+    }
+      
+    $sqlFirstQuery .= $sql;
+    $sqlSecondQuery .= $sql;
+    
     if($index != ''){
       $i = $index*10;
-      $sql .= "OFFSET $i;"; 
+      $sqlFirstQuery .= "LIMIT 10 OFFSET $i;";
+    } else {
+      $sqlFirstQuery .= ";";
     }
-    else 
-      $sql .= ";";
-    $result = mysqli_query($link,$sql);
-
-    if (!$result) {
+    $sqlSecondQuery .= ") as resultsCount;";
+    
+    $result = mysqli_query($link,$sqlFirstQuery);
+    $resultTotal = mysqli_query($link, $sqlSecondQuery);
+    
+    if (!$result || !$resultTotal) {
       http_response_code(404);
       die(mysqli_error());
     }
@@ -38,7 +67,12 @@ class GetUpcomingEvent {
     while($r = mysqli_fetch_assoc($result)) {
       $rows[] = $r;
     }
-    print json_encode($rows);
+    
+    $object->results = $rows;
+    while($r = mysqli_fetch_assoc($resultTotal)){
+      $object->totalResults = $r['totalResults'];
+    }
+    print json_encode($object);
 
     mysqli_close($link);
     exit();
