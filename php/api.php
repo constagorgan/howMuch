528f668fe95ad4659eb7c27ae5e8c01001727812
Justin Atanasiu <justin.atanasiu@gmail.com>
2017-03-20 21:14:43 +0200

    sign up send mail for sign up

--- php/api.php
+++ php/api.php
@@ -1,5 +1,5 @@
 <?php
-
+date_default_timezone_set('Europe/Bucharest');
 include "router.php";
 include "searchEvents.php";
 include "getEvent.php";
--- php/saveUser.php
+++ php/saveUser.php
@@ -1,4 +1,7 @@
 <?php
+
+include_once 'functions.php';
+
 class SaveUser {
   
   public static function saveUsers(){    
@@ -13,9 +16,9 @@ class SaveUser {
 
     $name = mysqli_real_escape_string($link, $name);
     if($data['email'] && $data['username'] && $data['password']){
-      $sql = "INSERT INTO `users` (`email`, `username`, `password`) VALUES ('".$data['email']."', '".$data['username']."', '".$data['password']."');";
+      $sql = "INSERT INTO `users` (`email`, `username`, `password`, `active`) VALUES ('".$data['email']."', '".$data['username']."', '".$data['password']."', 0);";
       $result = mysqli_query($link,$sql);
-
+      
       if (!$result) {
         if(mysqli_errno($link) == 1062)
           http_response_code(409);
@@ -23,6 +26,25 @@ class SaveUser {
           http_response_code(400);
       }
       
+//      else {
+        //put info into an array to send to the function
+        include_once 'swift/swift_required.php';
+        $info = array(
+            'username' => $data['username'],
+            'email' => $data['email'],
+            'key' => $data['username'] . $data['email'] . date('mY')
+        );
+
+        //send the email
+        if(send_email($info)){
+            //email sent
+            $action['result'] = 'success';
+
+        }else{
+            $action['result'] = 'error';
+
+        } 
+//      }
       mysqli_close($link);
     } 
     exit();
