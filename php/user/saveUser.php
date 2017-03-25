d333c582d64600d2bd37c5ab7cde9268d7af0b5c
Justin Atanasiu <justin.atanasiu@gmail.com>
2017-03-25 21:21:39 +0200

    Verify email is valid on backend on sign up

--- php/user/saveUser.php
+++ php/user/saveUser.php
@@ -14,46 +14,50 @@ class SaveUser {
 
     if($data['email'] && $data['username'] && $data['password']){
       $email = mysqli_real_escape_string($link, $data['email']);
-      $username = mysqli_real_escape_string($link, $data['username']);
-      $password = mysqli_real_escape_string($link, $data['password']);
-      $sql = "INSERT INTO `users` (`email`, `username`, `password`, `active`) VALUES ('$email', '$username', '$password', 0);";
-      $result = mysqli_query($link,$sql);
-      
-      if (!$result) {
-        if(mysqli_errno($link) == 1062)
-          http_response_code(409);
-        else
-          http_response_code(400);
+      if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+        http_response_code(400);
+      } else {
+        $username = mysqli_real_escape_string($link, $data['username']);
+        $password = mysqli_real_escape_string($link, $data['password']);
+        $sql = "INSERT INTO `users` (`email`, `username`, `password`, `active`) VALUES ('$email', '$username', '$password', 0);";
+        $result = mysqli_query($link,$sql);
+
+        if (!$result) {
+          if(mysqli_errno($link) == 1062)
+            http_response_code(409);
+          else
+            http_response_code(400);
+        }
+
+        else {
+          $userid = mysqli_insert_id($link);
+          //create a random key
+          $key = $username . $email . $password . date('mmY');
+          $key = md5($key);
+
+          //add confirm row
+          include(dirname(__DIR__).'/conf/config.inc.php'); 
+          $confirm = mysqli_query($link, "INSERT INTO `confirm_user` VALUES(NULL,'$userid','$key','$email')"); 
+          //put info into an array to send to the function
+          include_once 'swift/swift_required.php';
+          $info = array(
+              'username' => $username,
+              'email' => $email,
+              'key' => $key
+          );
+
+          //send the email
+          if(send_signup_email($info)){
+              //email sent
+              $action['result'] = 'success';
+
+          }else{
+              $action['result'] = 'error';
+
+          } 
+        }
+        mysqli_close($link);
       }
-      
-      else {
-        $userid = mysqli_insert_id($link);
-        //create a random key
-        $key = $username . $email . $password . date('mmY');
-        $key = md5($key);
-        
-        //add confirm row
-        include(dirname(__DIR__).'/conf/config.inc.php'); 
-        $confirm = mysqli_query($link, "INSERT INTO `confirm_user` VALUES(NULL,'$userid','$key','$email')"); 
-        //put info into an array to send to the function
-        include_once 'swift/swift_required.php';
-        $info = array(
-            'username' => $username,
-            'email' => $email,
-            'key' => $key
-        );
-
-        //send the email
-        if(send_signup_email($info)){
-            //email sent
-            $action['result'] = 'success';
-
-        }else{
-            $action['result'] = 'error';
-
-        } 
-      }
-      mysqli_close($link);
     } 
     exit();
   }
