b98ae4087352e25e30d8c8572e85edd3857761cc
Justin Atanasiu <justin.atanasiu@gmail.com>
2017-03-28 21:06:15 +0300

    Chat authorize without token => can’t send message, history not loaded twice

--- js/views/common/chatview.js
+++ js/views/common/chatview.js
@@ -65,24 +65,22 @@ define([
     
     $(function () {
       var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE0OTA2MzAwNDQsImp0aSI6Imd0dnhzU2w3XC8xUmRPSXZ6WlgwZGZpMEZadVVveWdcL3FmQ3pwekkwNkQyYz0iLCJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMSIsIm5iZiI6MTQ5MDYzMDA1NCwiZXhwIjoxNDkxMjM0ODU0LCJkYXRhIjp7ImlkIjoiNTUiLCJuYW1lIjoidGVzdGFzdCJ9fQ.taoAR1mBIfw7zIPwn--VOrcWKUhv4wDdCpBeS7qcy5g";
-      socket = io.connect('http://localhost:8081');
-      socket.on('connect', function () {
-        socket.emit('authenticate', {token: token}) //send the jwt
-      });
-      socket.on('authenticated', function () {
-        socket.emit('adduser', options.id + '_' + options.name)
+      socket = io.connect('http://localhost:8081', {
+        'query': 'token=' + token
       })
-      socket.on('unauthorized', function(msg) {
-        console.log("unauthorized: " + JSON.stringify(msg.data))
+      socket.on('connect', function () {
+        socket.emit('adduser', options.id + '_' + options.name)
       })
       socket.on('updatechat', function (username, data, date) {
           $('#chat_messages').append(getMessage(username, data, date));
           $('#conversation').scrollTop($('#conversation')[0].scrollHeight);
-      });
-
+      })
+      
       socket.on('updatehistory', function(history){
-        _.each(history, function(hist){$('#chat_messages').append(getMessage(hist.user, hist.content, hist.created))})
-        $('#conversation').scrollTop($('#conversation')[0].scrollHeight)
+        var sentMessagesBeforeReset = $('.chat-body-message-li');
+        if(!sentMessagesBeforeReset || !sentMessagesBeforeReset.length){
+          _.each(history, function(hist){$('#chat_messages').append(getMessage(hist.user, hist.content, hist.created))})
+        }
       })
       socket.on('disconnect', function(){
         //reset connection = > no more update history? 
@@ -91,7 +89,7 @@ define([
   }
 
   function getMessage(username, data, date) {
-    return '<li>' +
+    return '<li class="chat-body-message-li">' +
       '<div class="chat-body clearfix">' +
       '<div class="chat_header">' +
       '<strong class="primary-font">' + username + '</strong> <small class="pull-right text-muted">' +
