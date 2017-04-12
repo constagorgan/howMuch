b8ced16b34f8a6442f9e81e3365a7fa18cc16cd4
Justin Atanasiu <justin.atanasiu@gmail.com>
2017-04-12 11:56:44 +0300

    partial commit sign up modal

--- js/views/common/headerview.js
+++ js/views/common/headerview.js
@@ -16,13 +16,26 @@ define([
       'click #signUpButton': 'showSignUpModal',
       'click #reset_password_tab': 'showResetTab',
       'click #sign_in_tab': 'showSignInTab',
-      'click #sign_up_tab': 'showSignUpTab'
+      'click #sign_up_tab': 'showSignUpTab',
+      'focusin #datePicker': 'removeOverflowFromSignUpModal',
+      'focusout #datePicker': 'addOverflowToSignUpModal'
     },
     goToMainPage: function() {
       Backbone.history.navigate('#', true)
     },
     getRandomEvent: function () {
       common.getRandomEvent()
+    },
+    addOverflowToSignUpModal: function() {
+      $('.sign_up_form').addClass('overflowAuto')
+      $('.sign_up_form').removeClass('overflowHidden')
+    },
+    removeOverflowFromSignUpModal: function(){
+      $('.sign_up_form').removeClass('overflowAuto')
+      $('.sign_up_form').addClass('overflowHidden')
+    },
+    scrollSignUpFormTop: function(){
+      $('.sign_up_form').animate({ scrollTop: 0 }, 200)
     },
     showSideMenu: function () {
       $('#side_menu').css('margin-left', '0')
@@ -36,14 +49,16 @@ define([
       $('.reset_password_form_container').addClass("sign_up_tabs_rotate_zero")
     },
     showSignInTab: function(){
-      $('.sign_up_form').removeClass('overflowAuto')
+      this.scrollSignUpFormTop()
+      this.removeOverflowFromSignUpModal()
       this.hideResetTab()
     },
     showSignUpTab: function(){
-      $('.sign_up_form').addClass('overflowAuto')
+      this.addOverflowToSignUpModal()
       this.hideResetTab()
     },
     hideResetTab: function(){
+      this.scrollSignUpFormTop()
       var resetTab = $('.sign_up_tabs_rotate_zero')
       if(resetTab && resetTab.length){
         resetTab.removeClass('sign_up_tabs_rotate_zero')
