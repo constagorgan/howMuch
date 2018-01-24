94777cda940339018d29816dcd0ba59b5d089836
Justin Atanasiu <justin.atanasiu@gmail.com>
2018-01-24 18:24:41 +0200

    add display ad adsense

--- js/views/timerview/crawler.js
+++ js/views/timerview/crawler.js
@@ -62,12 +62,11 @@ define([
         ad += '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>' +
           '<ins class="adsbygoogle"' +
                'style="display:block"' +
-               'data-ad-format="fluid"' +
-               'data-ad-layout-key="-ee+4s+gk-c5-sd"' +
                'data-ad-client="ca-pub-7339410185917998"' +
-               'data-ad-slot="7396637315"></ins>' +
+               'data-ad-slot="8380570014"' +
+               'data-ad-format="auto"></ins>' +
           '<script>' +
-               '(adsbygoogle = window.adsbygoogle || []).push({});' +
+          '(adsbygoogle = window.adsbygoogle || []).push({});' +
           '</script>'
       } else {
         ad += 'No ad. This is not production.'
