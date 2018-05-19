70347f85b34a486c03b3e14e3a4e8f058478de4b
Justin Atanasiu <justin.atanasiu@gmail.com>
2018-05-20 00:04:14 +0300

    delete old user requests active 0 but no confirm_user old or bug fix

--- php/user/deleteOldUserRequests.php
+++ php/user/deleteOldUserRequests.php
@@ -5,7 +5,7 @@ $configs = include(dirname(__FILE__) . '/../config.php');
 
 $link = mysqli_connect($configs->myUltimateSecret, $configs->myBiggerSecret, $configs->myExtremeSecret, $configs->mySecret);
 
-$sql = "DELETE users, confirm_user FROM users INNER JOIN confirm_user ON users.id = confirm_user.userid  WHERE confirm_user.expirationDate < NOW() - INTERVAL 1 MONTH AND users.active = 0";
+$sql = "DELETE users, confirm_user FROM users LEFT JOIN confirm_user ON users.id = confirm_user.userid  WHERE (confirm_user.expirationDate < NOW() - INTERVAL 1 MONTH OR confirm_user.userid IS NULL) AND users.active = 0";
 $stmt = $link->prepare($sql);
 $stmt->execute();
 $result = $stmt->get_result();
